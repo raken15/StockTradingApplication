14724da720be6e3905a3dfa16430b85c9995a6b6
Raviv <raken100@gmail.com>
2024-10-11 00:11:55 +0300

    Moving the view files to the correct folder Views and updating the global application setting file App.xaml to run the Views from the correct path

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace StockTradingApplication;
+namespace StockTradingApplication.Views;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
