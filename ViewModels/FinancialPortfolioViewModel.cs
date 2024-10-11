17ae01551b5ac819aaa1f9e82ce7e8dce45a1d93
Raviv <raken100@gmail.com>
2024-10-11 14:31:12 +0300

    implemented the FinancialPortfolioViewModel

--- ViewModels/FinancialPortfolioViewModel.cs
+++ ViewModels/FinancialPortfolioViewModel.cs
@@ -1,10 +1,37 @@
 using System;
+using System.ComponentModel;
 using StockTradingApplication.Models;
 
 namespace StockTradingApplication.ViewModels;
 
-public class FinancialPortfolioViewModel
+public class FinancialPortfolioViewModel : INotifyPropertyChanged
 {
     private FinancialPortfolioModel _model;
+    public List<StockViewModel> StocksPorfolio { get; set; }
+    private decimal _money; 
+    public decimal Money
+    {
+        get { return _money; }
+        set
+        {
+            if (_money != value)
+            {
+                _money = value;
+                RaisePropertyChanged(nameof(Money));
+            }
+        }
+    }
+    public FinancialPortfolioViewModel(FinancialPortfolioModel model)
+    {
+        _model = model;
+        StocksPorfolio = _model.Stocks.Select(s => new StockViewModel(s)).ToList();
+        _money = _model.Money;
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void RaisePropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 
 }
