d61f4c3a3a40310dac084e62df4f6f3a8730bae0
Raviv <raken100@gmail.com>
2024-10-16 19:03:06 +0300

    Added all the logic for buying and selling stocks with the financial portfolio

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -10,8 +10,11 @@ namespace StockTradingApplication.ViewModels
     {
         private readonly IRepository<StockModel, string> _stockRepository;
         private StockViewModel _selectedStock;
+        private FinancialPortfolioViewModel _financialPortfolio;
+        private StockViewModel _selectedPortfolioStock;
         public ObservableCollection<StockViewModel> Stocks { get; set; }
         public RelayCommand BuyStockCommand { get; }
+        public RelayCommand SellStockCommand { get; }
         public StockViewModel SelectedStock
         {
             get => _selectedStock;
@@ -24,14 +27,45 @@ namespace StockTradingApplication.ViewModels
                 }
             }
         }
-
+        public FinancialPortfolioViewModel FinancialPortfolio
+        {
+            get { return _financialPortfolio; }
+            set
+            {
+                if (_financialPortfolio != value)
+                {
+                    _financialPortfolio = value;
+                    RaisePropertyChanged(nameof(FinancialPortfolio));
+                }
+            }
+        }
+        public StockViewModel SelectedPortfolioStock
+        {
+            get => _selectedPortfolioStock;
+            set
+            {
+                if (_selectedPortfolioStock != value)
+                {
+                    _selectedPortfolioStock = value;
+                    SellStockCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
         public MainViewModel()
         {
             _stockRepository = new StockModelRepository();
             Stocks = new ObservableCollection<StockViewModel>();
             InitializeStocks();
 
+            var financialPortfolioModel = new FinancialPortfolioModel
+            {
+                Money = 1000.0m,
+                Stocks = new List<StockModel>()
+            };
+            FinancialPortfolio = new FinancialPortfolioViewModel(financialPortfolioModel);
+
             BuyStockCommand = new RelayCommand(async (param) => await BuyStockAsync(), (param) => CanBuyStock());
+            SellStockCommand = new RelayCommand(async (param) => await SellStockAsync(), (param) => CanSellStock());
         }
         private void InitializeStocks()
         {
@@ -45,19 +79,51 @@ namespace StockTradingApplication.ViewModels
         {
             if (SelectedStock != null)
             {
-                await Task.Run(() => 
+                // Simulated stock buying logic
+                SelectedStock.Quantity--;
+
+                await Task.Yield(); // Yield to the UI thread
+                if(FinancialPortfolio.StocksPortfolio.Any(x => x.Symbol == SelectedStock.Symbol))
                 {
-                    // Simulated stock buying logic
-                    SelectedStock.Quantity--;
-                    RaisePropertyChanged(nameof(Stocks));
-                });
+                    FinancialPortfolio.StocksPortfolio.First(x => x.Symbol == SelectedStock.Symbol).Quantity++;
+                }
+                else
+                {
+                    FinancialPortfolio.StocksPortfolio.Add(new StockViewModel(
+                    new StockModel() { Symbol = SelectedStock.Symbol,
+                    Quantity = 1, Price = SelectedStock.Price }));
+                }
+                FinancialPortfolio.Money -= SelectedStock.Price;
+
+                RaisePropertyChanged(nameof(Stocks));
+                BuyStockCommand.RaiseCanExecuteChanged();
             }
-            BuyStockCommand.RaiseCanExecuteChanged();
+        }
+        private async Task SellStockAsync()
+        {
+            if (SelectedPortfolioStock != null)
+            {
+                await Task.Yield(); // Yield to the UI thread
+                // Simulated stock buying logic
+                SelectedPortfolioStock.Quantity--;
+                FinancialPortfolio.Money += SelectedPortfolioStock.Price;
+                if(Stocks.Any(x => x.Symbol == SelectedPortfolioStock.Symbol))
+                {
+                    Stocks.First(x => x.Symbol == SelectedPortfolioStock.Symbol).Quantity++;
+                }
+                //RaisePropertyChanged(nameof(Stocks));
+                SellStockCommand.RaiseCanExecuteChanged();
+            }
         }
         private bool CanBuyStock()
         {
             return SelectedStock != null && SelectedStock.Quantity > 0; // Can only buy if a stock is selected and has quantity
         }
+        private bool CanSellStock()
+        {
+            return SelectedPortfolioStock != null && SelectedPortfolioStock.Quantity > 0; // Can only buy if a stock is selected and has quantity
+        }
+
          public event PropertyChangedEventHandler PropertyChanged;
          private void RaisePropertyChanged(string propertyName)
         {
